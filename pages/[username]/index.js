afba62b00eb94f8fbe94c5ac90d269c9d2321a35
reub-eN <36566445+reub-eN@users.noreply.github.com>
2022-04-10 17:08:37 +0100

    fix(user): parse user data on user page

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -22,18 +22,19 @@ export async function getServerSideProps({ query }) {
  
 
   if (userDoc) {
-    user = userDoc;
-    console.log(userDoc)
-    doc(collection(db, 'usernames'),userDoc)
+    // user = userDoc;
+    user = userDoc.docs[0].data();
+    // console.log(userDoc)
+    // doc(collection(db, 'usernames'),userDoc)
 
-    const docRef =  collection(db,userDoc,'designs')
-    console.log(docRef)
+    // const docRef =  collection(db,userDoc,'designs')
+    // console.log(docRef)
 
-    const designsQuery = query(docRef,where('published', '==', true))
-    .orderBy('createdAt', 'desc')
-    .limit(5)
+    // const designsQuery = query(docRef,where('published', '==', true))
+    // .orderBy('createdAt', 'desc')
+    // .limit(5)
 
-    designs = (await designsQuery.get()).docs.map(designToJSON);
+    // designs = (await designsQuery.get()).docs.map(designToJSON);
   }
   
   return {

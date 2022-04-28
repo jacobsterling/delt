6c1567ee6078b30bce4b76e19b50828410f51ff1
quikc <j10.sterling@gmail.com>
2022-04-28 09:12:40 +0100

    design feed done

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -6,8 +6,7 @@ const designs = await client.from("designs")
 </script>
 
 <template>
-  <div v-for="design in designs" class="mx-auto px-4 dark:text-white max-w-screen-xl">
-    {{ design }}
-    <!-- <DesignCard :design="design" /> -->
+  <div v-for="design in designs.data" class="mx-auto px-4 dark:text-white max-w-screen-xl">
+    <DesignCard :design="design" />
   </div>
 </template>
